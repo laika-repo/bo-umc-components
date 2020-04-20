8b93bbb9f1a4b45044b598484bef246bb5b71dc0
Jeroen Janssen <toproper@icloud.com>
2020-04-20 10:57:16 +0200

    Removed shadow dom/css scoping from umc-logo and umc-title-block

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -3,7 +3,7 @@ import { Component, Host, h, Prop } from '@stencil/core';
 @Component({
   tag: 'umc-logo',
   styleUrl: 'logo.scss',
-  shadow: true
+  shadow: false
 })
 export class Logo {
   @Prop() src = "logo-aumc.svg";
--- src/components/title-block/title-block.tsx
+++ src/components/title-block/title-block.tsx
@@ -3,7 +3,7 @@ import { Component, Host, Prop, h } from '@stencil/core';
 @Component({
   tag: 'umc-title-block',
   styleUrl: 'title-block.scss',
-  shadow: true
+  shadow: false
 })
 export class TitleBlock {
   @Prop() title: string;
